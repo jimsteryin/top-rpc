fd32a759acf2a9527005e97ff07dbfbeab4d8946
jimmy <jimmyyin94@gmail.com>
2022-05-04 10:48:42 -0700

    play function complete, add tie scenario

--- javascript.js
+++ javascript.js
@@ -15,27 +15,33 @@ function computerPlay(){
 
 function play(playerSelection, computerSelection) {
     //nested if statements for each playerSelection
-
-    if(playerSelection.lower() === "rock"){
+    let result;
+    if(playerSelection.toLowerCase() === computerSelection){
+        result = "Tie! Both chose " + computerSelection + ".";
+    }
+    if(playerSelection.toLowerCase() === "rock"){
         switch(computerSelection){
-            case "rock":
             case "paper":
+                result = "You Lose! Paper beats Rock";
             case "scissors":
+                result = "You Win! Rock beats Scissors";
         }
     }
-    else if(playerSelection.lower() === "paper"){
+    else if(playerSelection.toLowerCase() === "paper"){
         switch(computerSelection){
             case "rock":
-            case "paper":
+                result = "You Win! Paper beats Rock";
             case "scissors":
+                result = "You Lose! Scissors Beats Rock";
         }
     }
     else{
         switch(computerSelection){
             case "rock":
+                result = "You Lose! Rock beats Scissors";
             case "paper":
-            case "scissors":
+                result = "You Win! Scissors beats Paper";
         }
     }
-
+    return result;
 }
