b930a19da501e4d57f17c1af3453873a6a14b9d4
jimmy <jimmyyin94@gmail.com>
2022-05-09 13:54:54 -0500

    Complete game code

--- javascript.js
+++ javascript.js
@@ -4,44 +4,90 @@ function computerPlay(){
 
     //return R/P/C based on random integer
     switch (rand){
-        case 1:
+        case 0:
             return "rock";
-        case 2:
+        case 1:
             return "paper";
-        case 3:
+        case 2:
             return "scissors";
     }
 }
 
-function play(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
     //nested if statements for each playerSelection
     let result;
+    let winner;
     if(playerSelection.toLowerCase() === computerSelection){
         result = "Tie! Both chose " + computerSelection + ".";
     }
-    if(playerSelection.toLowerCase() === "rock"){
-        switch(computerSelection){
-            case "paper":
-                result = "You Lose! Paper beats Rock";
-            case "scissors":
-                result = "You Win! Rock beats Scissors";
-        }
-    }
-    else if(playerSelection.toLowerCase() === "paper"){
-        switch(computerSelection){
-            case "rock":
-                result = "You Win! Paper beats Rock";
-            case "scissors":
-                result = "You Lose! Scissors Beats Rock";
-        }
-    }
     else{
-        switch(computerSelection){
-            case "rock":
-                result = "You Lose! Rock beats Scissors";
-            case "paper":
-                result = "You Win! Scissors beats Paper";
+        if(playerSelection.toLowerCase() === "rock"){
+            switch(computerSelection){
+                case "paper":
+                    result = "You Lose! Paper beats Rock";
+                    winner = "computer";
+                case "scissors":
+                    result = "You Win! Rock beats Scissors";
+                    winner = "player";
+            }
+        }
+        else if(playerSelection.toLowerCase() === "paper"){
+            switch(computerSelection){
+                case "rock":
+                    result = "You Win! Paper beats Rock";
+                    winner = "player"
+                case "scissors":
+                    result = "You Lose! Scissors Beats Rock";
+                    winner = "computer";
+            }
+        }
+        else{
+            switch(computerSelection){
+                case "rock":
+                    result = "You Lose! Rock beats Scissors";
+                    winner = "computer";
+                case "paper":
+                    result = "You Win! Scissors beats Paper";
+                    winner = "player";
+            }
         }
     }
-    return result;
+    console.log(result);
+    return winner;
+}
+
+function game(){
+    let playerWin = 0;
+    let computerWin = 0;
+    let ties = 0;
+    let player;
+    let winner;
+
+
+    for(let i = 0; i < 5; i++){
+        let invalid = true;
+        while(invalid){
+            player = prompt("Rock Paper or Scissors");
+            if(player.toLowerCase() === "rock" || player.toLowerCase() === "paper" || player.toLowerCase() === "scissors"){
+                invalid = false;
+            }
+            else {
+                invalid = true;
+            }
+        }
+
+        winner = playRound(player, computerPlay());
+        if(winner === "player"){
+            playerWin++;
+        }
+        else if(winner === "computer"){
+            computerWin++;
+        }
+        else{
+            ties++;
+        }
+
+        console.log(playerWin, computerWin, ties);
+    }
+    return;
 }
